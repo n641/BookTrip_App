8867bde0d60433b1ce5359842c5f6d633850a8fe
Noha Mohammed <noha67357@gmail.com>
2023-08-07 14:06:57 +0300

    fix

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    env: {
-      production: {
-        plugins: ['react-native-paper/babel',
-          'react-native-reanimated/plugin', {
-            relativeSourceLocation: true,
-          },
-        ],
-      },
-    },
+    plugins: [
+      'react-native-paper/babel',
+      'react-native-reanimated/plugin',
+    ],
   };
 };
